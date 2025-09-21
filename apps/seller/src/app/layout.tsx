dbcc1186d184552172937b655373d25b6dabd0b6
Rengumin <tranxuanphong26525@gmail.com>
2025-09-21 16:02:03 +0700

    refactor: remove unused Loading component and clean up imports in layout

--- apps/seller/src/app/layout.tsx
+++ apps/seller/src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ReactNode, Suspense } from "react";
+import { ReactNode } from "react";
 import AjaxProgressBarProvider from "@/features/ajax-progress-bar/provider";
-import { AuthProvider } from "@/features/auth";
 import { Toaster } from "@/components/ui/sonner";
 import ReactQueryProvider from "@/features/react-query/provider";
-import Loading from "./loading";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
