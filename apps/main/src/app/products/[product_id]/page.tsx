ebd2a57f7a0789386cc25d2ef2133b85d9bd7f3d
Rengumin <tranxuanphong26525@gmail.com>
2025-09-21 16:21:55 +0700

    feat: partial integrate real data to products detail page

--- apps/main/src/app/products/[product_id]/page.tsx
+++ apps/main/src/app/products/[product_id]/page.tsx
@@ -1,12 +1,4 @@
-import {notFound} from "next/navigation";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator
-} from "@/components/ui/breadcrumb";
+import { notFound } from "next/navigation";
 
 import ProductImages from "@/features/products/detail/product-image";
 import RatingStars from "@/components/ui/rating-stars";
@@ -16,63 +8,38 @@ import CustomerReviews from "@/features/products/detail/customer-review/customer
 import ProductDescription from "@/features/products/detail/product-description";
 import ShopInfoCard from "@/features/products/detail/shop-info-card";
 import ProductService from "@/features/products/service";
-import Link from 'next/link';
-export default async function ProductPage({params}: { params: Promise<{ product_id: string }> }) {
-    const {product_id} = await params;
+import ProductBreadcrumb from "@/features/products/detail/product-breadcrumb";
+export default async function ProductPage({ params }: { params: Promise<{ product_id: string }> }) {
+    const { product_id } = await params;
     if (!product_id) {
         return notFound();
     }
     const product = await ProductService.getProductById(product_id);
-
     console.log(product)
     return (
         <div className="max-w-7xl my-4 mx-auto">
-            <Breadcrumb className=" px-4 mb-4">
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link href="/public" className="text-custom-1/80 hover:text-custom-1">Shopiew</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator/>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link href="/src/componentsents"
-                                  className="text-custom-1/80 hover:text-custom-1">Components</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator/>
-                    <BreadcrumbItem className="flex-1">
-                        <BreadcrumbPage className="line-clamp-1 text-ellipsis">
-                            Sét áo blazer dáng suông ve mèo 5B07.2408K, Chân váy đè dây gấu A 4S27.2408K th
-                            mmmmmmmmmmmmmmmmmmmiết kế bởi JM Dress Design b;a bla bal
-                        </BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
+            <ProductBreadcrumb product={product} />
             <div className="sm:flex relative">
                 <div className=" flex-1">
                     <div className=" bg-white rounded-2xl shadow-sm p-4">
                         <div className="sm:flex ">
-                            <ProductImages/>
+                            <ProductImages />
                             <div className={"flex-1 sm:ml-4"}>
                                 <h1 className="text-lg">
-                                    Sét áo blazer dáng suông ve mèo 5B07.2408K, Chân váy đè dây gấu A 4S27.2408K thiết
-                                    kế bởi JM Dress Design b;a bla bal
-
+                                    {product.name}
                                 </h1>
 
                                 <div
                                     className="flex items-center mb-2 hover:bg-gray-50/10 w-fit p-2 -mx-2 rounded-md ">
-                                <span
-                                    className="ml-3 text-lg">{3.15}
-                                </span>
+                                    <span
+                                        className="ml-3 text-lg">{3.15}
+                                    </span>
                                     <a href="#">
-                                        <RatingStars rating={3}/>
+                                        <RatingStars rating={3} />
                                     </a>
                                     <span
                                         className="text-sm text-gray-600 dark:text-gray-300">({31513})
-                                </span>
+                                    </span>
                                     <span className="ml-2 opacity-50">|</span>
                                     <span
                                         className="ml-2 font-sans  text-sm">352 sold</span>
@@ -89,19 +56,19 @@ export default async function ProductPage({params}: { params: Promise<{ product_
                                         Shop coupons
                                     </h2>
                                     <div className="ml-2 flex gap-2 text-red-500 font-bold text-sm">
-                                    <span className="bg-red-200/80 px-2 py-1 rounded-sm ">
-                                        5% OFF
-                                    </span>
                                         <span className="bg-red-200/80 px-2 py-1 rounded-sm ">
-                                        5k OFF
-                                    </span>
+                                            5% OFF
+                                        </span>
                                         <span className="bg-red-200/80 px-2 py-1 rounded-sm ">
-                                        7% OFF
-                                    </span>
+                                            5k OFF
+                                        </span>
+                                        <span className="bg-red-200/80 px-2 py-1 rounded-sm ">
+                                            7% OFF
+                                        </span>
                                     </div>
                                 </div>
 
-                                <ShopInfoCard/>
+                                <ShopInfoCard />
 
                             </div>
                         </div>
@@ -109,14 +76,14 @@ export default async function ProductPage({params}: { params: Promise<{ product_
 
                     </div>
                     <div className="bg-white rounded-2xl shadow-sm  mt-4">
-                        <ProductSpecifications/>
-                        <ProductDescription/>
-                        <CustomerReviews/>
-                    </div>
+                        <ProductSpecifications specs={product.specs} />
+                        <ProductDescription description={product.description} />
+                        <CustomerReviews />
+                    </div> 
                 </div>
 
                 <div className="sticky top-24 w-[280px] ml-0 sm:ml-4 bg-white rounded-2xl shadow-sm p-4 h-fit">
-                    <ProductAction/>
+                    <ProductAction />
                 </div>
             </div>
 
--- apps/main/src/features/products/detail/product-specifications.tsx
+++ apps/main/src/features/products/detail/product-specifications.tsx
@@ -1,28 +1,16 @@
-export default  function ProductSpecifications(){
+export default  function ProductSpecifications({specs}:{specs: Record<string, string>}) {
     return (
         <div className="overflow-hidden ">
             <h2 className="text-lg font-medium p-4 bg-muted/50">Product Specifications</h2>
             <div className="divide-y text-sm">
-                <div className="grid grid-cols-3 p-4">
-                    <span className="text-muted-foreground">Category</span>
-                    <span>Bluetooth speakers</span>
-                </div>
-                <div className="grid grid-cols-3 p-4">
-                    <span className="text-muted-foreground">Stock</span>
-                    <span>2259</span>
-                </div>
-                <div className="grid grid-cols-3 p-4">
-                    <span className="text-muted-foreground">Bluetooth Function</span>
-                    <span>Yes</span>
-                </div>
-                <div className="grid grid-cols-3 p-4">
-                    <span className="text-muted-foreground">Connection Type</span>
-                    <span>Wireless</span>
-                </div>
-                <div className="grid grid-cols-3 p-4">
-                    <span className="text-muted-foreground">Ships From</span>
-                    <span>Binh Duong</span>
-                </div>
+                {
+                    Object.entries(specs).map(([key, value]) => (
+                        <div className="grid grid-cols-3 p-4" key={key}>
+                            <span className="text-muted-foreground">{key}</span>
+                            <span>{value}</span>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
