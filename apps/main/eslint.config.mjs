d1408e5ef50b359dea09a8a1431362a75631d2c2
Rengumin <tranxuanphong26525@gmail.com>
2026-01-07 23:11:34 +0700

    refactor: upgrade to nextjs16

--- apps/main/eslint.config.mjs
+++ apps/main/eslint.config.mjs
@@ -1,26 +1,22 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Disable any type errors
-      "@typescript-eslint/no-explicit-any": "off",
-      // Disable unused vars errors
-      "@typescript-eslint/no-unused-vars": "off",
-      // Also disable the standard no-unused-vars rule
-      "no-unused-vars": "off"
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    // Disable any type errors
+    "@typescript-eslint/no-explicit-any": "off",
+    // Disable unused vars errors
+    "@typescript-eslint/no-unused-vars": "off",
+    // Also disable the standard no-unused-vars rule
+    "no-unused-vars": "off"
   },
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
